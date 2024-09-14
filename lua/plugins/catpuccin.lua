0e993c86b8324247100e0b368243db84e3da5302
fusionjed <fusionjed>
2024-09-15 01:23:13 +0800

    initialize

---
+++ lua/plugins/catpuccin.lua
@@ -0,0 +1,15 @@
+return {
+  "catppuccin/nvim",
+  name = "catppuccin",
+  opts = {
+    -- configuration options...
+    transparent_background = true,
+    flavour = "mocha",
+    custom_highlights = {
+      TabLineFill = { bg = "#1e1e2e" },
+    },
+    integrations = {
+      neotree = false,
+    },
+  },
+}
