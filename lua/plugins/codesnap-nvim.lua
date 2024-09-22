657d8ced8e86b968a7e0c22b4ed32728819388b7
fusionjed <fusionjed>
2024-09-22 19:35:55 +0800

    reproduced CodeSnap issue(it's fine now)

--- lua/plugins/codesnap-nvim.lua
+++ lua/plugins/codesnap-nvim.lua
@@ -1,17 +1,15 @@
 return {
-  {
-    "mistricky/codesnap.nvim",
-    build = "make build_generator",
-    keys = {
-      { "<leader>cc", "<cmd>CodeSnap<cr>", mode = "x", desc = "Save selected code snapshot into clipboard" },
-      { "<leader>cs", "<cmd>CodeSnapSave<cr>", mode = "x", desc = "Save selected code snapshot in ~/Pictures" },
-    },
-    opts = {
-      save_path = "~/Pictures",
-      bg_theme = "grape",
-      has_breadcrumbs = true,
-      has_line_numbers = true,
-      watermark = "@FusionJed",
-    },
+  "mistricky/codesnap.nvim",
+  build = "make build_generator",
+  keys = {
+    { "leader>cc", "<cmd>CodeSnap<cr>", mode = "x", desc = "Save selected code snapshot into clipboard" },
+    { "<leader>cs", "<cmd>CodeSnapSave<cr>", mode = "x", desc = "Save selected code snapshot in ~/Pictures" },
+  },
+  opts = {
+    save_path = "~/Pictures",
+    bg_theme = "grape",
+    has_breadcrumbs = true,
+    has_line_numbers = true,
+    watermark = "@FusionJed",
   },
 }
